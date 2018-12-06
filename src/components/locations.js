b4f3ac85d10ee55ecda6e09c98f9c0b4ad04ea92
Matthew Smith <mateoelcule@gmail.com>
2018-12-05 22:01:24 -0800

    Added 'locationsList' component.

---
+++ src/components/locations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class locationsList extends React.Component {
+     render() {
+          return (
+               <p>List</p>
+          )
+     }
+}
+
+export default locationsList;
