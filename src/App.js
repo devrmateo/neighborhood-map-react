00cd91688512cb433b09b358ffa6d5c30b6d4b22
Matthew Smith <mateoelcule@gmail.com>
2018-12-16 11:22:34 -0800

    Wrote functions to fetch data from FourSquare and render map.  Locations are being successfully fetched and the map is rendering correctly.  All work done in main 'App' component.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import LocationsList from './components/locations';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    locations: [],
+    lat: 34.15334,
+    lng: -118.761676
+  }
+
+  componentDidMount = () => {
+    fetch('https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=40.7243,-74.0018&near=Agoura Hills, CA')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.response.groups[0].items);
+      const locations = data.response.groups[0].items;
+      this.setState({
+        locations
+      });
+    })
+    .then(() => this.displayMap())
+    .catch((error) => console.log(error));
+  }
+
+  displayMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?libraries=geometry&key=AIzaSyBYxtGxA3B4KgCSBExLmK_lD_lq5u-xkMA&callback=initMap");
+    window.initMap = this.initMap;
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+    center: {lat: 34.15334, lng: -118.761676},
+    zoom: 12
+  });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <LocationsList />
+        <div id="map"></div>
       </div>
     );
   }
+}
+
+function loadScript(url) {
+  const firstScriptTag = window.document.getElementsByTagName('script')[0];
+  const script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
 }
 
 export default App;
---
+++ src/components/locations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './locations.css';
+class LocationsList extends React.Component {
+     render() {
+          return (
+               <div class="locationsWrapper">
+                    <h3>Locations</h3>
+               </div>
+               );
+     }
+}
+
+export default LocationsList;
