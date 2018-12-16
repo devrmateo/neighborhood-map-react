c312b4aac236116f86a587db27015ba612bb4bdc
Matthew Smith <mateoelcule@gmail.com>
2018-12-16 15:28:42 -0800

    Passed the locations and the 'filterLocations' function from the App component to the locations component.  Locations in the location list are now filtering correctly.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ class App extends Component {
 
   state = {
     locations: [],
+    filteredLocations: [],
     lat: 34.15334,
-    lng: -118.761676
+    lng: -118.761676,
+    markers: []
   }
 
   componentDidMount = () => {
-    fetch('https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=40.7243,-74.0018&near=Agoura Hills, CA')
+    fetch(`https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=${this.state.lat},${this.state.lng}`)
     .then((response) => {
       return response.json();
     })
@@ -19,7 +21,8 @@ class App extends Component {
       console.log(data.response.groups[0].items);
       const locations = data.response.groups[0].items;
       this.setState({
-        locations
+        locations,
+        filteredLocations: this.filterLocations(locations, '')
       });
     })
     .then(() => this.displayMap())
@@ -31,18 +34,55 @@ class App extends Component {
     window.initMap = this.initMap;
   }
 
-  initMap = () => {
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-    center: {lat: 34.15334, lng: -118.761676},
-    zoom: 12
-  });
+  updateLocations = (query) => {
+    this.setState({
+      filteredLocations: this.filterLocations(this.state.locations, query),
+    });
   }
 
+  filterLocations = (locations, query) => {
+    return locations.filter((location) => location.venue.name.toLowerCase().includes(query.toLowerCase()));
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: this.state.lat,
+      lng: this.state.lng},
+      zoom: 12
+    });
+
+    const largeInfowindow = new window.google.maps.InfoWindow();
+
+    const markers = this.state.markers;
+
+    for (let i = 0; i < this.state.locations.length; i++) {
+      // Get the position from the location array.
+      const position = {
+        lat: this.state.locations[i].venue.location.lat,
+        lng: this.state.locations[i].venue.location.lng
+      };
+      const title = this.state.locations[i].name;
+      // Create a marker per location, and put into markers array.
+      const marker = new window.google.maps.Marker({
+        map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: this.state.locations[i].venue.id
+      });
+      // Push the marker to our array of markers.
+      markers.push(marker);
+    }
+  }
 
   render() {
     return (
       <div className="App">
-        <LocationsList />
+        <LocationsList
+          locations={this.state.filteredLocations}
+          filterLocations={this.updateLocations}
+        />
         <div id="map"></div>
       </div>
     );
--- src/components/locations.js
+++ src/components/locations.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './locations.css';
 class LocationsList extends React.Component {
+
+     state = {
+          query: ''
+     }
+
+     updateQuery = (query) => {
+          this.setState({
+               query
+          });
+          this.props.filterLocations(query);
+     }
+
      render() {
 
           const locations = this.props.locations;
@@ -8,6 +20,11 @@ class LocationsList extends React.Component {
           return (
                <div className="locationsWrapper">
                     <h3 className="locationsHeader">Locations</h3>
+                    <input
+                         type="text"
+                         value={this.state.query}
+                         onChange={(e) => this.updateQuery(e.target.value)}
+                    />
                     <ul className="locationsList">
                          {locations.map((location) => {
                               return <li
