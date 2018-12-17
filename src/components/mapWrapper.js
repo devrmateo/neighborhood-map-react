c01aec97e7b7b83cf4120b3bfddea9f30f574c26
Matthew Smith <mateoelcule@gmail.com>
2018-12-17 11:46:04 -0800

    Wrote 'closeInfoWindow', 'onMarkerClick', and 'updateMarkers' methods in 'MapWrapper.js'.  Imported the InfoWindow component from 'google-maps-react' and included it in the render method.  Location markers now loading correctly.

--- src/components/mapWrapper.js
+++ src/components/mapWrapper.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
 import {Marker,} from 'google-maps-react/dist/components/Marker';
+import './mapWrapper.css';
 
 export class MapWrapper extends React.Component {
      state = {
@@ -19,8 +20,68 @@ export class MapWrapper extends React.Component {
           this.updateMarkers(this.props.locations);
      }
 
-     updateMarkers = (locations) => {
+     closeInfoWindow = () => {
+          this.state.activeMarker && this.state.activeMarker.setAnimation(null);
+          this.setState({
+               showingInfoWindow: false,
+               activeMarker: null,
+               activeMarkerProps: null
+          })
+     }
 
+     onMarkerClick = (props, marker, e) => {
+          this.closeInfoWindow();
+
+          this.setState({
+               showingInfoWindow: true,
+               activeMarker: marker,
+               activeMarkerProps: props
+          });
+     }
+
+     updateMarkers = (locations) => {
+          if (!locations) {
+               return;
+          }
+          this.state.markers.forEach((marker) => {
+               marker.setMap(null);
+          })
+
+          let markerProps = [];
+          let markers = locations.map((location, index) => {
+               let position = {
+                    lat: location.location.lat,
+                    lng: location.location.lng
+               }
+
+               let mProps = {
+                    key: index,
+                    index,
+                    name: location.name,
+                    title: location.name,
+                    position: position,
+               }
+
+               markerProps.push(mProps);
+
+               let animation = this.props.google.maps.Animation.DROP;
+               let marker = this.props.google.maps.Marker({
+                    map: this.state.map,
+                    position: location.position,
+                    animation
+               });
+
+               marker.addListener('click', () => {
+                    this.onMarkerClick(mProps, marker, null);
+               });
+
+               return marker;
+          });
+
+          this.setState({
+               markers,
+               markerProps
+          });
      }
 
      render() {
@@ -32,21 +93,22 @@ export class MapWrapper extends React.Component {
                lat: lat,
                lng: lng
           }
-          const style = {
-               height: '100vh',
-               width: '100vw'
-          }
+
+          let amProps = this.state.activeMarkerProps;
 
           return (
 
+               <div
+                    id="map"
+                    role="application"
+                    aria-label="map"
+               >
                     <Map
-                         role="application"
-                         aria-label="map"
-                         style={style}
                          onReady={this.mapReady}
                          google={this.props.google}
                          initialCenter={center}
                          zoom={13}
+                         onClick={this.closeInfoWindow}
                     >
 
                     {locations.map((location) => {
@@ -63,9 +125,16 @@ export class MapWrapper extends React.Component {
                               />)
                     })}
 
+                         <InfoWindow
+                              marker={this.state.activeMarker}
+                              visible={this.state.showingInfoWindow}
+                              onClose={this.closeInfoWindow}>
+                              <div>
+                                  <h3>{amProps && amProps.name}</h3>
+                              </div>
+                         </InfoWindow>
                     </Map>
-
-
+               </div>
 
           )
      }
