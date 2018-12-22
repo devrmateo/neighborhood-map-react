297b13a316c1590ef22c6fe5a879f307ee9dca02
Matthew Smith <mateoelcule@gmail.com>
2018-12-22 10:14:41 -0800

    Rewrote the app from scratch, without using any external libraries to load the Google map.  Map now loading successfully, location markers showing, infowindows are loading correctly, and filter function is working as intended.

--- src/App.js
+++ src/App.js
@@ -1,97 +1,135 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LocationsList from './components/locations';
-import MapWrapper from './components/mapWrapper';
+import Map from './components/mapDisplay';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
 
   state = {
+    map: null,
+    query: '',
     locations: [],
-    filteredLocations: null,
+    filteredLocations: [],
     lat: 34.15334,
     lng: -118.761676,
-    selectedId: null,
-    selectedMarker: null
+    markers: []
   }
 
   componentDidMount = () => {
     fetch(`https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=${this.state.lat},${this.state.lng}`)
-    .then((response) => response.json()
-    )
-    .then((data) => {
-      let fetchedLocations = [];
-      for (let i = 0; i < data.response.groups[0].items.length; i++) {
-        fetchedLocations.push(data.response.groups[0].items[i].venue)
-      }
-      return fetchedLocations;
+    .then((response) => {
+      return response.json();
     })
-    .then((fetchedLocations) => {
-      const locations = fetchedLocations;
+    .then((data) => {
+      const locations = data.response.groups[0].items;
+      //Set the filtered location list to be equal to the location list to begin with, so that the list will populate before any filtering is done.
       this.setState({
         locations,
-        filteredLocations: this.filterLocations(locations, "")
+        filteredLocations: locations
       });
     })
-    .catch((error) => alert("There was an error loading FourSquare data."));
+    .then(() => this.displayMap())
+    .catch((error) => console.log(error));
   }
 
-  updateQuery = (query) => {
+  displayMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?libraries=geometry&key=AIzaSyBYxtGxA3B4KgCSBExLmK_lD_lq5u-xkMA&callback=initMap");
+    window.initMap = this.initMap;
+  }
+
+  filterLocations = (query) => {
+    let locations;
+    if (query) {
+      locations = this.state.locations.filter((location) => location.venue.name.toLowerCase().includes(query.toLowerCase()));
+    } else {
+      locations = this.state.locations;
+    }
+
+    this.state.markers.forEach((marker) => {
+      if (marker.title.toLowerCase().includes(query.toLowerCase())) {
+        marker.setMap(this.state.map);
+      } else {
+        marker.setMap(null);
+      }
+    });
+
     this.setState({
-      selectedIndex: null,
-      filteredLocations: this.filterLocations(this.state.locations, query)
+      filteredLocations: locations,
+      query
     });
   }
 
-  filterLocations = (locations, query) => {
-    return locations.filter((location) => location.name.toLowerCase().includes(query.toLowerCase()));
-  }
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: this.state.lat,
+      lng: this.state.lng},
+      zoom: 12
+    });
 
-  markers = []
+    this.setState({
+      map
+    })
 
-  addMarker = (marker) => {
-    if (this.markers.indexOf(marker) === -1 && marker !== null) {
-      this.markers.push(marker);
+    const largeInfowindow = new window.google.maps.InfoWindow();
+
+    const markers = this.state.markers;
+
+    for (let i = 0; i < this.state.locations.length; i++) {
+      // Get the position from the location array.
+      const position = {
+        lat: this.state.locations[i].venue.location.lat,
+        lng: this.state.locations[i].venue.location.lng
+      };
+      const title = this.state.locations[i].venue.name;
+      // Create a marker per location, and put into markers array.
+      const marker = new window.google.maps.Marker({
+        map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: this.state.locations[i].venue.id
+      });
+      // Push the marker to our array of markers.
+      markers.push(marker);
+      marker.addListener('click', () => {
+        this.populateInfoWindow(map, marker, largeInfowindow);
+          });
     }
   }
 
-  clickMarker = (id) => {
-    const marker = this.markers.filter((marker) => marker.marker.id === id)[0];
-    console.log(marker);
-    this.setState({
-      selectedId: id,
-      selectedMarker: marker
-    });
-  }
-
-  clickListLocation = (index, id) => {
-    this.clickMarker(id);
-    this.setState({
-      selectedIndex: index
-    });
+  populateInfoWindow = (map, marker, infowindow) => {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker !== marker) {
+      // Clear the infowindow content to give the streetview time to load.
+      infowindow.setContent(`${marker.title}`);
+      infowindow.marker = marker;
+      // Make sure the marker property is cleared if the infowindow is closed.
+      // Open the infowindow on the correct marker.
+      infowindow.open(map, marker);
+    }
   }
 
   render() {
     return (
       <div className="App">
         <LocationsList
-          selectedMarker={this.state.selectedMarker}
           locations={this.state.filteredLocations}
-          filterLocations={this.updateQuery}
-          clickMarker={this.clickMarker}
-          clickListLocation={this.clickListLocation}
+          filterLocations={this.filterLocations}
         />
-        <MapWrapper
-          locations={this.state.filteredLocations}
-          lat={this.state.lat}
-          lng={this.state.lng}
-          addMarker={this.addMarker}
-          clickMarker={this.clickMarker}
-          selectedMarker={this.state.selectedMarker}
-          selectedIndex={this.state.selectedIndex}
-        />
+        <Map />
       </div>
     );
   }
+}
+
+function loadScript(url) {
+  const firstScriptTag = window.document.getElementsByTagName('script')[0];
+  const script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
 }
 
 export default App;
--- src/components/locations.js
+++ src/components/locations.js
@@ -1,52 +1,30 @@
 import React from 'react';
 import './locations.css';
-
-
 class LocationsList extends React.Component {
 
-     state = {
-          query: ''
-     }
-
-     updateQuery = (query) => {
-          this.setState({
-               query: query
-          });
-          this.props.filterLocations(query);
-     }
-
      render() {
+
           const locations = this.props.locations;
 
           return (
-               <div className="locationsListWrapper">
-                    <h3>Locations</h3>
+               <div className="locationsWrapper">
+                    <h3 className="locationsHeader">Locations</h3>
                     <input
                          type="text"
-                         placeholder="Filter List"
-                         onChange={(e) => this.updateQuery(e.target.value)}
-                         value={this.state.query}
+                         value={this.props.query}
+                         onChange={(e) => this.props.filterLocations(e.target.value)}
                     />
                     <ul className="locationsList">
-                         {locations && locations.map((location, index) => {
-                              return (
-                                   <li
-                                        className="locationsListItem"
-                                        key={index}
-                                   >
-                                        <button
-                                             key={index}
-                                             index={index}
-                                             onClick={(e) => this.props.clickListLocation(index, location.id)}
-                                        >
-                                             {location.name}
-                                        </button>
-                                   </li>
-                              )
+                         {locations && locations.map((location) => {
+                              return <li
+                                        key={location.venue.id}
+                                        className="listItem">
+                              {location.venue.name}
+                              </li>
                          })}
                     </ul>
                </div>
-          )
+               );
      }
 }
 
