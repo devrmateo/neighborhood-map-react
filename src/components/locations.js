2e033c635ec53c622999aac24300deebc3864e3f
Matthew Smith <mateoelcule@gmail.com>
2018-12-27 17:45:58 -0800

    Added FontAwesome hamburger icon, and wrote styling that hides the location list at smaller screen sizes.

--- src/components/locations.js
+++ src/components/locations.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import './locations.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
 class LocationsList extends React.Component {
 
      render() {
 
           const {locations} = this.props;
 
+
           return (
-               <div className="locationsWrapper">
-                    <h3 className="locationsHeader">Locations</h3>
-                    <p className="fourSquare"><em>Locations provided by FourSqure</em></p>
+               <div className="locations">
+                  <FontAwesomeIcon
+                    icon={faBars}
+                    className="fa"
+                  />
+                  <div className="listWrapper">
                     <input
                          type="text"
                          value={this.props.query}
                          placeholder="Filter Locations"
                          onChange={(e) => this.props.filterLocations(e.target.value)}
                     />
+                    <p className="fourSquare"><em>Locations provided by FourSqure</em></p>
                     <nav>
                          <ul className="locationsList">
                               {locations && locations.map((location) =>
@@ -30,8 +39,9 @@ class LocationsList extends React.Component {
                                                   </button>
                                              </li>
                                         })}
-                         </ul>
-                    </nav>
+                              </ul>
+                         </nav>
+                    </div>
                </div>
           );
      }
