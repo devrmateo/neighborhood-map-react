7decb4c32064d42250757be8689ecddfb3c30472
Matthew Smith <mateoelcule@gmail.com>
2018-12-30 17:58:46 -0800

    Began the process of refactoring the incorporation of the Google Map using promises, so as to eliminate the presence of the global window reference to the map.

--- src/App.js
+++ src/App.js
@@ -2,38 +2,62 @@ import React, { Component } from 'react';
 import LocationsList from './components/locations';
 import Map from './components/mapDisplay';
 import './App.css';
+import {getGoogleMaps, getPlaces} from './utils';
 
 class App extends Component {
 
   state = {
-    map: null,
     query: '',
-    locations: [],
-    filteredLocations: [],
+    filtered: [],
     lat: 34.15334,
     lng: -118.761676,
-    markers: [],
   }
 
   componentDidMount = () => {
-    fetch(`https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=${this.state.lat},${this.state.lng}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const locations = data.response.groups[0].items;
-      this.setState({
-        locations,
-        filteredLocations: locations
-      });
-    })
-    .then(() => this.displayMap())
-    .catch((error) => console.log(error));
-  }
+    let googleMapsPromise = getGoogleMaps();
+    let placesPromise = getPlaces(this.state.lat, this.state.lng);
 
-  displayMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?libraries=geometry&key=AIzaSyBYxtGxA3B4KgCSBExLmK_lD_lq5u-xkMA&callback=initMap");
-    window.initMap = this.initMap;
+    Promise.all([
+      googleMapsPromise,
+      placesPromise
+      ])
+      .then((values) => {
+        let google = values[0];
+        let places = values[1].response.groups[0].items;
+        let venues = [];
+        places.forEach((place) => {
+          const venue = place.venue;
+          venues.push(venue);
+        })
+        this.google = google;
+        this.markers = [];
+        this.infowindow = new google.maps.InfoWindow();
+        this.map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: {
+            lat: this.state.lat,
+            lng: this.state.lng
+          }
+        });
+        venues.forEach((venue) => {
+          const marker = new google.maps.Marker({
+            map: this.map,
+            position: {lat: venue.location.lat, lng: venue.location.lng},
+            title: venue.name,
+            animation: window.google.maps.Animation.DROP,
+            id: venue.id,
+        });
+      // Push the marker to our array of markers.
+        this.markers.push(marker);
+        marker.addListener('click', () => {
+          this.populateInfoWindow(this, this.infowindow);
+        });
+        });
+        this.setState({
+          filtered: venues,
+        })
+        console.log(this.state.filtered);
+      });
   }
 
   filterLocations = (query) => {
@@ -159,29 +183,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="title">Around the Neighborhood</h1>
-        <h4 className="subtitle">Things to do in Agoura Hills, California</h4>
-        <LocationsList
-          locations={this.state.filteredLocations}
-          filterLocations={this.filterLocations}
-          onListItemClick={this.onListItemClick}
-          toggleDrawer={this.toggleDrawer}
-        />
-        <Map
-          closeDrawer={this.closeDrawer}
-        />
+        <LocationsList />
+        <Map />
       </div>
     );
   }
-}
-
-function loadScript(url) {
-  const firstScriptTag = window.document.getElementsByTagName('script')[0];
-  const script = window.document.createElement('script');
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-  firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
 }
 
 export default App;
---
+++ src/utils.js
@@ -0,0 +1,21 @@
+export function getGoogleMaps() {
+  return new Promise((resolve, reject) => {
+    window.resolveGoogleMapsPromise = () => {
+      resolve(window.google);
+      delete window.resolveGoogleMapsPromise;
+    };
+    const script = document.createElement('script');
+    const API = 'AIzaSyBYxtGxA3B4KgCSBExLmK_lD_lq5u-xkMA';
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&callback=resolveGoogleMapsPromise`;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  });
+}
+
+export function getPlaces(lat, lng) {
+     return fetch(`https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=${lat},${lng}`)
+    .then((response) => {
+      return response.json();
+    })
+}
